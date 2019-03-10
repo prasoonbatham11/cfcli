1c19a50736f5d437e8344a323691b38ab69acf02
prasoonbatham11 <prasoonbatham@gmail.com>
2019-03-10 22:20:59 +0530

    Minor changes

--- cf/__init__.py
+++ cf/__init__.py
@@ -1,11 +1,1 @@
-from .user_info import *
-from .user_rating import *
-from .contest_list import *
-from .problems import *
-from .blog import *
-from .ratingchange import *
-from .bloguser import *
-from .userstatus import *
-from .conteststatus import *
-from .compare import *
 __version__ = '1.0.0'
--- cf/__main__.py
+++ cf/__main__.py
@@ -2,6 +2,16 @@ import sys
 import argparse
 import requests
 import json
+from .user_info import *
+from .user_rating import *
+from .contest_list import *
+from .problems import *
+from .blog import *
+from .ratingchange import *
+from .bloguser import *
+from .userstatus import *
+from .conteststatus import *
+from .compare import *
 
 def get_req(url):
     return requests.get(url)

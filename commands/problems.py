e95365b76e6e83d446998157cf3c0ba1ecd2cd7c
prasoonbatham11 <prasoonbatham@gmail.com>
2019-03-10 08:11:49 +0530

    Minor changes

--- commands/problems.py
+++ commands/problems.py
@@ -37,12 +37,12 @@ def prob_stat(i):
     title = header[0]('div', attrs={'class':'title'})[0].get_text()
     time_limit = header[0]('div', attrs={'class':'time-limit'})[0].get_text()[19:]
     mem_limit = header[0]('div', attrs={'class':'memory-limit'})[0].get_text()[21:]
-
+    
     statement = pstate('div')[10]('p')
     state = []
     for s in statement:
         state.append(s.get_text().replace('$$$', ''))
-
+    
     input_spec = pstate('div', {'class':'input-specification'})[0]('p')
     input_s = []
     for s in input_spec:
@@ -62,26 +62,26 @@ def prob_stat(i):
     for s in sample_test_o:
         sto.append(s.get_text().replace('Output',''))
 
-    paged = get_colored("\t|"+title, "blue")+"\n"
-    paged += get_colored("\t|Time: ",'blue')+get_colored(time_limit, color='magenta')+"\n"
-    paged += get_colored("\t|Memory: ",'blue')+get_colored(mem_limit, color='magenta')+"\n\n\n"
+    paged = get_colored("\t"+title, "blue")+"\n"
+    paged += get_colored("\tTime: ",'blue')+get_colored(time_limit, color='magenta')+"\n"
+    paged += get_colored("\tMemory: ",'blue')+get_colored(mem_limit, color='magenta')+"\n\n\n"
 
-    paged += get_colored("\t|Problem Statement:\n", 'blue')+"\n"
+    paged += get_colored("\tProblem Statement:\n", 'blue')+"\n"
     for s in state:
-        paged += get_colored("\t|"+"\n\t|".join(textwrap.wrap(s, 150))+"\n")+"\n"
+        paged += get_colored("\t"+"\n\t".join(textwrap.wrap(s, 150))+"\n")+"\n"
 
-    paged += get_colored("\t|Input:\n", 'blue')+"\n"
+    paged += get_colored("\tInput:\n", 'blue')+"\n"
     for s in input_s:
-        paged += get_colored("\t|"+"\n\t|".join(textwrap.wrap(s, 150))+"\n")+"\n"
+        paged += get_colored("\t"+"\n\t".join(textwrap.wrap(s, 150))+"\n")+"\n"
 
-    paged += get_colored("\t|Output:\n", 'blue')+"\n"
+    paged += get_colored("\tOutput:\n", 'blue')+"\n"
     for s in output_s:
-        paged += get_colored("\t|"+"\n\t|".join(textwrap.wrap(s, 150))+"\n")+"\n"
+        paged += get_colored("\t"+"\n\t".join(textwrap.wrap(s, 150))+"\n")+"\n"
 
     for i in range(len(sti)):
-        paged += get_colored("\t|Sample Input "+str(i)+":", 'blue')+"\n"
+        paged += get_colored("\tSample Input "+str(i)+":", 'blue')+"\n"
         paged += get_colored("\t "+sti[i].replace('\n', '\n\t '), 'cyan')+"\n"
-        paged += get_colored("\t|Sample Output "+str(i)+":", 'blue')+"\n"
+        paged += get_colored("\tSample Output "+str(i)+":", 'blue')+"\n"
         paged += get_colored("\t "+sto[i].replace('\n', '\n\t '), 'green')+"\n"
     
     pydoc.pager(paged)
